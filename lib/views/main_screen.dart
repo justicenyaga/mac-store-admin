bff576710cf1f5a9607509561da7d2815eff43bf
Justice Nyaga <justiceevansmwema@gmail.com>
2025-06-01 18:42:44 +0300

    create main screen appbar and sidebar

--- lib/views/main_screen.dart
+++ lib/views/main_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_admin_scaffold/admin_scaffold.dart';
 
@@ -12,7 +13,46 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return AdminScaffold(
-      body: Text("Dashboard"),
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: const Text("Management"),
+      ),
+      sideBar: const SideBar(
+        items: [
+          AdminMenuItem(
+            title: "Vendors",
+            route: "",
+            icon: CupertinoIcons.person_3,
+          ),
+          AdminMenuItem(
+            title: "Buyers",
+            route: "",
+            icon: CupertinoIcons.person,
+          ),
+          AdminMenuItem(
+            title: "Orders",
+            route: "",
+            icon: CupertinoIcons.shopping_cart,
+          ),
+          AdminMenuItem(
+            title: "Categories",
+            route: "",
+            icon: Icons.category,
+          ),
+          AdminMenuItem(
+            title: "Upload Banner",
+            route: "",
+            icon: Icons.upload,
+          ),
+          AdminMenuItem(
+            title: "Products",
+            route: "",
+            icon: Icons.store,
+          ),
+        ],
+        selectedRoute: "",
+      ),
+      body: const Text("Dashboard"),
     );
   }
 }
