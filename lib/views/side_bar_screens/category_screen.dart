8190098b48509f52087d4acaf5cc42b269fee212
Justice Nyaga <justiceevansmwema@gmail.com>
2025-06-01 19:34:35 +0300

    implement category screen UI

--- lib/views/side_bar_screens/category_screen.dart
+++ lib/views/side_bar_screens/category_screen.dart
@@ -1,13 +1,88 @@
 import 'package:flutter/material.dart';
 
-class CategoryScreen extends StatelessWidget {
+class CategoryScreen extends StatefulWidget {
   static const String id = "\category-screen";
   const CategoryScreen({super.key});
 
   @override
+  State<CategoryScreen> createState() => _CategoryScreenState();
+}
+
+class _CategoryScreenState extends State<CategoryScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Category Screen"),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Text(
+            "Categories",
+            style: TextStyle(
+              fontSize: 36,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        const Padding(
+          padding: EdgeInsets.all(4),
+          child: Divider(
+            color: Colors.grey,
+          ),
+        ),
+        Row(
+          children: [
+            Container(
+              width: 150,
+              height: 150,
+              decoration: BoxDecoration(
+                color: Colors.grey,
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: const Center(
+                child: Text("Category image"),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SizedBox(
+                width: 200,
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: "Enter Category Name",
+                  ),
+                ),
+              ),
+            ),
+            TextButton(
+              onPressed: () {},
+              child: const Text("Cancel"),
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
+              ),
+              onPressed: () {},
+              child: const Text(
+                "Save",
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ElevatedButton(
+            onPressed: () {},
+            child: const Text("Pick image"),
+          ),
+        ),
+        const Divider(
+          color: Colors.grey,
+        ),
+      ],
     );
   }
 }
